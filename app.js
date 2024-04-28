c4799d20c8b4015e12c92d72175df746de1a617d
Lazreg Mohamed <108418392+MAL-WD@users.noreply.github.com>
2024-04-28 02:17:02 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes');
 
 // express app
 const app = express();
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true })); // this will take all the url encoded data and pass it as an object that we can use int req object //for accepting form data
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use((req, res, next) => {
   res.locals.path = req.path;
@@ -35,55 +35,7 @@ app.get('/about', (req, res) => {
 });
 
 // blog routes
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
-
-app.get('/blogs', (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
-    .then(result => {
-      res.render('index', { blogs: result, title: 'All blogs' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.post('/blogs', (req, res) => {
-  // console.log(req.body);
-  const blog = new Blog(req.body);
-
-  blog.save()
-    .then(result => {
-      res.redirect('/blogs'); // so this will redirect them to the home page after they submit the form
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-// route paramaters :the variables part of the route that may change value /blogs/:id we add :
-app.get('/blogs/:id', (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then(result => {
-      res.render('details', { blog: result, title: 'Blog Details' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.delete('/blogs/:id', (req, res) => {
-  const id = req.params.id;
-  
-  Blog.findByIdAndDelete(id) // to find and deleta at the same time
-    .then(result => {
-      res.json({ redirect: '/blogs' }); // when we send ajax request we can't use redirect "redirect()" as a reponse so we send it as a json property
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
+app.use('/blogs', blogRoutes); // we look at all the routes on the blogRoutes and apply it on the app instead
 
 // 404 page
 app.use((req, res) => {
